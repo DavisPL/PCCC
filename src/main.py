35c57eab53349f51220d662270670d7003cdd4b6
Parniaan <pkamran@ucdavis.edu>
2023-09-26 09:20:38 -0700

    Added code validations

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import sys
+from components import core
+def main():
+    print("Proof Carrying LLMs")
+    print("There is nothing here!")
+    
+if __name__ == '__main__':
+    core.llm_core()
+    sys.exit(main())  
