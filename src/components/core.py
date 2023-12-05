af28c922762287807f07ed703c78b0e7e51375ca
Parniaan <pkamran@ucdavis.edu>
2023-12-04 23:53:38 -0800

    :wrench: Modify validator to compile dafny code

--- src/components/core.py
+++ src/components/core.py
@@ -1,62 +1,105 @@
+"""
+Core includes LLM models
+Gets prompt from the user 
+
+Returns
+-------
+_type_
+    _description_
+"""
+import json
+import os
+
 import openai
+from dotenv import load_dotenv
+
 
 class LlmCore:
     def __init__(self):
         pass
-    
-    def get_api_key(self, api_key):
-        openai.api_key = open(api_key, "r").read().strip('\n')
-        
+
+    # Open the file containing the API key
+    def get_api_key(self, api_key_file):
+        load_dotenv()
+        """Reads api key from file"""
+        openai.api_key = os.environ["OPENAI_API_KEY"]
+        return openai.api_key
+
     def get_prompt(self, prompt_path):
-        
         try:
-            with open(prompt_path, 'r') as file:
+            with open(prompt_path, "r") as file:
                 content = file.read()
-                prompts = content.split('---\n')
+                prompts = content.split("---\n")
                 prompts = [prompt.strip() for prompt in prompts if prompt.strip()]
         except FileNotFoundError:
+            ## TODO: Provide a format for prompt and pass it to the function
             print(f"Error: The file {prompt_path} does not exist.")
             prompts = []
         except IOError as e:
-                print(f"Error reading the file {prompt_path}: {str(e)}")
-                prompts = []
+            print(f"Error reading the file {prompt_path}: {str(e)}")
+            prompts = []
         return prompts
-    
 
     def request_code(self, prompt, generated_code_file):
+        ## TODO Get model specifications from config in the function args
         message_history = []
         responses = []
+        # client = O
         try:
-            message_history.append({
-            'role': 'system', 
-            'content': prompt
-            }),
+            message_history.append(
+                # {"role": "system", "content": "You are a helpful code generator tool"},
+                {"role": "user", "content": prompt}
+            ),
             completion = openai.ChatCompletion.create(
                 model="gpt-4",
-                messages= message_history,
+                messages=message_history,
                 max_tokens=4000,  # Adjustable number of tokens to control response length
                 n=1,
                 stop=None,
                 temperature=0.5,
-        )
-            reply_content = completion.choices[0].message.content
-            print(reply_content)
-        
-            code_start = reply_content.find('```rust')
-            code_end = reply_content.rfind('```')
-            
-            if code_start == -1 or code_end == -1 or code_start == code_end:
-                return None  # Could not find the code in the response
-        
-            code = reply_content[code_start + len('```rust'):code_end].strip()
-            responses.append(reply_content)
-            message_history.append({'role': 'assistant', 'content': reply_content})
-        except openai.error.OpenAIError as e:
-            print(f"Error encountered for prompt {str(e)}")
-        
-        with open(generated_code_file, "w") as file:
-            file.write(code)
+                # response_format={"type": "json_object"},
+            )
+            try:
+                ##TODO Add config file for the model specifications
+                # response = completion.choices[0].message.content
+                content = completion.choices[0].message.content
+                print(f"LLM generated_text {content}")
+                response_json = json.loads(content)
+                # print(f"LLM responds {response_json}")
 
-        return code
+                # code_start = response.find("```")
+                # code_end = response.rfind("```")
 
+                # if code_start == -1 or code_end == -1 or code_start == code_end:
+                #     return None  # Could not find the code in the response
 
+                # code = response[code_start + len("```") : code_end].strip()
+                responses.append(content)
+                code = response_json["code"]
+                print(f"code ??? {code}")
+                # code_start = code.find("```")
+                # code_end = code.rfind("```")
+
+                # if code_start == -1 or code_end == -1 or code_start == code_end:
+                #     return None  # Could not find the code in the response
+
+                # code = code[code_start + len("```") : code_end]
+                message_history.append({"role": "assistant", "content": content})
+            except openai.error.OpenAIError as e:
+                print(f"Error encountered for prompt {str(e)}")
+
+            try:
+                with open(generated_code_file, "w", encoding="utf-8") as f:
+                    print(f"Code {code}")
+                    f.write(code)
+                    f.close()
+            except FileNotFoundError:
+                print(f"Cannot find {generated_code_file}")
+
+            except IOError as e:
+                print(f"Error writing into the file {generated_code_file}: {str(e)}")
+
+            return code
+        except Exception as e:
+            print(f"An error occured in the API: {e}")
+            return None
--- src/components/validator.py
+++ src/components/validator.py
@@ -3,18 +3,37 @@ import time
 
 from components import core
 
+
 class Validator:
     def __init__(self):
-      pass
-    
+        pass
+
+    # Add validator for dafny
+    def compile_dafny_code(self, generated_code_file):
+        try:
+            subprocess.run(
+                ["dafny", generated_code_file],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            return None
+        except subprocess.CalledProcessError as e:
+            return e.stderr.decode()
+
     def compile_rust_code(self, generated_code_file):
         try:
             # Check the Rust code to see if it comiples or not
-            subprocess.run(["rustc", generated_code_file], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(
+                ["rustc", generated_code_file],
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             return None  # No error message since compilation succeeded
         except subprocess.CalledProcessError as e:
-            return e.stderr.decode() 
-        
+            return e.stderr.decode()
+
     def request_and_validate_code(self, api_key_file, prompt_path, generated_code_file):
         llm_core = core.LlmCore()
         llm_core.get_api_key(api_key_file)
@@ -22,13 +41,16 @@ class Validator:
         for _ in range(3):
             for prompt in prompts:
                 code = llm_core.request_code(prompt, generated_code_file)
+                print(f"Validator code is \n {code}")
                 if code is not None:
                     print(f"Received code:\n{code}")
                 else:
-                    print("Failed to extract code from GPT-3 response.")
-                
-                print("Attempting to compile the Rust code...")
-                error_message = self.compile_rust_code(generated_code_file)
+                    print("Failed to extract code from GPT-3 response. \n")
+
+                print("Attempting to compile the code... \n")
+                # error_message = self.compile_rust_code(generated_code_file)
+
+                error_message = self.compile_dafny_code(generated_code_file)
                 if error_message is None:
                     print("Compilation succeeded!")
                     return
@@ -36,6 +58,8 @@ class Validator:
                     print(f"Compilation failed with error:\n{error_message}")
                     # Modify the prompt with the error message to get a better response
                     current_prompt = f"{prompt}\n\nCompiler Error:\n{error_message}"
-                    time.sleep(1)  # Add a delay to avoid hitting the API rate limit too quickly
-                    with open(prompt_path, 'a') as prompts_file:
+                    time.sleep(
+                        1
+                    )  # Add a delay to avoid hitting the API rate limit too quickly
+                    with open(prompt_path, "a") as prompts_file:
                         prompts_file.write(f"\n---\n{current_prompt}")
