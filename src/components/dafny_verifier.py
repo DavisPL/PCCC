670cfb2774d0d5b0a063a4b2799a71c9834b6d0f
Parniaan <pkamran@ucdavis.edu>
2024-07-29 16:44:20 -0700

    updated

--- src/components/dafny_verifier.py
+++ src/components/dafny_verifier.py
@@ -44,10 +44,10 @@ def count_invariant(source):
     return len(occurrence)
 
 
-def count_assert(source):
-    patterns = "assert.*\n"
-    occurrence = re.findall(patterns, source)
-    return len(occurrence)
+# def count_assert(source):
+#     patterns = "assert.*\n"
+#     occurrence = re.findall(patterns, source)
+#     return len(occurrence)
 
 
 def count_ensures(source):
@@ -61,24 +61,38 @@ def count_requires(source):
     occurrence = re.findall(patterns, source)
     return len(occurrence)
 
+def check_filestream_usage(source):
+    pattern = r'\bvar\s+([a-zA-Z_]\w*)\s*:\s*FileStream\s*;?'
+    occurrence = re.findall(pattern, source)
+    return occurrence
+
+def check_filestream_open(source):
+    pattern = r'\b(ok)\s*,\s*([a-zA-Z_]\w*)\s*:=\s*FileStream\.Open\(\s*([a-zA-Z_]\w*)\s*\)\s*;?'
+    occurence = re.search(pattern, source)
+    return occurence
+    
+
 
 def get_all_verification_bits_count(code):
     obj = {}
-    obj['method'] = count_method(code)
+    # obj['method'] = count_method(code)
     obj['ensure'] = count_ensures(code)
     obj['requires'] = count_requires(code)
-    obj['function'] = count_function(code)
-    obj['lemma'] = count_lemma(code)
-    obj['predicate'] = count_predicate(code)
-    obj['invariant'] = count_invariant(code)
-    obj['assert_count'] = count_assert(code)
+    obj['filestream_usage'] = check_filestream_usage(code)
+    obj['filestream_open'] = check_filestream_open(code)
+    print("---------------------------------")
+    print("obj", obj)
+    # obj['function'] = count_function(code)
+    # obj['lemma'] = count_lemma(code)
+    # obj['predicate'] = count_predicate(code)
+    # obj['invariant'] = count_invariant(code)
+    # obj['assert_count'] = count_assert(code)
     return obj
 
 def get_verification_info(error):
     pass
 
 def get_verification_bits_count(path):
-    # code = utility.read_file(path)
     code = utils.read_file(path)
     return get_all_verification_bits_count(code)
 
@@ -94,8 +108,20 @@ def get_verification_bits_count_rq3(save_map):
     return get_all_verification_bits_count(code)
 
 
+
+def find_filestream_usage(source, error_msg):
+    pattern = r'\bvar\s+([a-zA-Z_]\w*)\s*:\s*FileStream\s*;?'
+    occurrence = re.findall(pattern, source)
+    if len(occurrence) == 0:
+        error_msg_filestream = ""
+    else:
+        error_msg_filestream = "FileStream Class declaration not found"
+    return error_msg.append(error_msg_filestream)
+
 def get_dafny_verification_result(dfy_file_path):
     cmd_output = ""
+    error_msg = []
+    find_filestream_usage(source, error_msg);
     try:
         cmd_output = check_output(["dafny", "verify", dfy_file_path], timeout=300, encoding='utf8')
     except TimeoutExpired as e:
