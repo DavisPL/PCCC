7ea1339d1ee3b5f94407cb2389cc0eaad18f7a49
Parniaan <pkamran@ucdavis.edu>
2024-08-07 11:32:08 -0700

    Fix misspelling in prompt

--- src/components/prompt_generator.py
+++ src/components/prompt_generator.py
@@ -17,7 +17,7 @@ class PromptGenerator:
         print(f"example_prompt_template: \n {example_prompt_template}")
        
         prompt = FewShotPromptTemplate(
-            prefix="SYSTEM:\nYou are an expert code assistant tasked with implementing Dafny code for filesystem operations. Your implementation should adhere to the following guidelines:\n - Must utilize given Safe APIs for file operations.\n- Generate Dafny code with appropriate preconditions to satisfy safe API preconditions.\n- Ensure that the code satisfies given safety properties for filesystem operations.\n- You are only limted to the provided method signatures and preconditions.",
+            prefix="SYSTEM:\nYou are an expert code assistant tasked with implementing Dafny code for filesystem operations. Your implementation should adhere to the following guidelines:\n - Must utilize given Safe APIs for file operations.\n- Generate Dafny code with appropriate preconditions to satisfy safe API preconditions.\n- Ensure that the code satisfies given safety properties for filesystem operations.\n- You are only limited to the provided method signatures and preconditions.",
             examples=examples,
             example_prompt=example_prompt_template,
             suffix='''TASK:\n{{task}}\n\nAI ASSISTANT:\n\n''',
